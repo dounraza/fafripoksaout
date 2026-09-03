5c3a1c0138d5272a8b98449285a1aec5095d8b37
zorazafi <razafimandimbydoun@gmail.com>
2026-09-03 10:48:33 +0300

    update react base url

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import api from './api';
 import { onlineUsersSocket } from '../engine/socket';
 // process.env.REACT_APP_BASE_URL ||
-const BASE_URL =  'http://localhost:5000';
+const BASE_URL =  process.env.REACT_APP_BASE_URL ||'http://localhost:5000';
 
 const API_URL = `${BASE_URL}/api/auth/login`; 
 
